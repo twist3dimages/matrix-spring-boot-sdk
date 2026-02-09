cf0996c9c16fa706b836accc2ffb09d112ed0e8d
Charley <baileycw15@gmail.com>
2026-02-09 06:06:59 +0000

    chore(deps): Update dependency com.michael-bull.kotlin-retry:kotlin-retry to v1.0.9

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val kotlin = "1.4.32" // https://kotlinlang.org/
-    const val kotlinRetry = "1.0.8" // https://github.com/michaelbull/kotlin-retry
+    const val kotlinRetry = "1.0.9" // https://github.com/michaelbull/kotlin-retry
     const val ktor = "1.5.4" // https://github.com/ktorio/ktor/releases
     const val kotest = "4.4.3" // https://github.com/kotest/kotest/releases
     const val springBoot = "2.4.5" // https://spring.io/projects/spring-boot
