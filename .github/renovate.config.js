6626a0f50f7c9a533758b42fd752ef67a0d113b1
Charley <33134502+twist3dimages@users.noreply.github.com>
2026-02-08 19:39:25 -0500

    dependabot

---
+++ .github/renovate.config.js
@@ -0,0 +1,19 @@
+﻿module.exports = {
+  // ...existing config...
+  automerge: true,
+  extends: ["config:recommended", ":dependencyDashboard"],
+  major: { automerge: true },
+  minor: { automerge: true },
+  patch: { automerge: true },
+  // Dynamically use GITHUB_REPOSITORY or default to a fallback value
+  repositories: [
+    process.env.GITHUB_REPOSITORY ||
+      "Estes-Forwarding-Worldwide/freshservice-offboarding-assets",
+  ],
+  packageRules: [
+    {
+      matchUpdateTypes: ["major", "minor", "patch", "pin", "digest"],
+      automerge: true,
+    },
+  ],
+};
