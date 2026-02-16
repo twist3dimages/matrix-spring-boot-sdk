e548d5efb6ba16d8c373ddcc9eef0558c8f05aa1
Charley <baileycw15@gmail.com>
2026-02-16 06:05:33 +0000

    chore(deps): Update ktor monorepo to v1.6.8

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val kotlin = "1.4.32" // https://kotlinlang.org/
     const val kotlinRetry = "1.0.9" // https://github.com/michaelbull/kotlin-retry
-    const val ktor = "1.5.4" // https://github.com/ktorio/ktor/releases
+    const val ktor = "1.6.8" // https://github.com/ktorio/ktor/releases
     const val kotest = "4.4.3" // https://github.com/kotest/kotest/releases
     const val springBoot = "2.4.5" // https://spring.io/projects/spring-boot
     const val springDependencyManagement =
